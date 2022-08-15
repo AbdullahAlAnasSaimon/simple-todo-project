208fe19e4ec52228d9450a9e235a3306d6a73034
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-08-15 21:35:24 +0600

    simple to-do app

---
+++ main.js
@@ -0,0 +1,26 @@
+/* const items = document.getElementsByClassName('in-out');
+    for(const item of items){
+      item.addEventListener('click', function(){
+        event.target.parentNode.removeChild(event.target);
+      })
+    } */
+    const listContainer = document.getElementById('in-out');
+    listContainer.addEventListener('click', function(event){
+      // event.target.parentNode.removeChild(event.target);
+      event.target.style.textDecoration = "line-through";
+    })
+    listContainer.addEventListener("dblclick", function(event){
+      event.target.parentNode.removeChild(event.target);
+    })
+
+    const button = document.getElementById('btn-task');
+    button.addEventListener('click', function(){
+      const listContainer = document.getElementById('in-out');
+
+      const li = document.createElement('li');
+      let inputValue = document.getElementById('text-input');
+      li.innerText = inputValue.value;
+      inputValue.value = '';
+      li.classList.add('item');
+      listContainer.appendChild(li);
+    })
